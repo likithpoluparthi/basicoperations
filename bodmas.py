9a71efe1287837faeed57a68983cbff488a39a42
likithpoluparthi <likith.poluparthi@dal.ca>
2019-11-19 22:30:27 -0400

    first attempt

---
+++ bodmas.py
@@ -0,0 +1,7 @@
+a= input("enter the first number")
+b=input("enter the second number")
+
+print "sum of the given two number is", a+b
+print "multiplication of the given two numbers is", a*b
+print "dividing the two numbers results in", a/b
+
